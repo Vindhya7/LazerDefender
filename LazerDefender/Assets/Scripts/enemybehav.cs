1b9465a3f9a21434ecff6c318d9c4f5c3a0c8457
Vindhya R <vindhyarao2013@gmail.com>
2018-01-03 11:23:37 +0530

    Destroy on hit

---
+++ LazerDefender/Assets/Scripts/enemybehav.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemybehav : MonoBehaviour {
+	
+	public float health=150f;
+
+	void OnTriggerEnter2D(Collider2D col){
+		projectile missile = col.gameObject.GetComponent<projectile> ();
+
+		if (missile) {
+			health -= missile.getdamage ();
+			missile.hit ();
+			if (health <= 0) {
+				Destroy (gameObject);
+			}
+
+		}
+	}
+}
---
+++ LazerDefender/Assets/Scripts/projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour {
+	public float damage=100f;
+
+	public float getdamage(){
+		return damage;
+	}
+
+	public void hit(){
+		Destroy (gameObject);
+	}
+
+
+}
